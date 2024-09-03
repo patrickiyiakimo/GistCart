a633f073ede4d395e6d27aa4d183ad8318961c8c
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-09-03 13:19:41 +0100

    added margin to the input fields

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -43,102 +43,107 @@ export default function Page() {
             Proceed Checkout
           </h1>
         </div>
-        <section className="ml-10 lg:justify-center lg:items-center lg:ml-52">
-          <p className="font-semibold mx-4 inline">Customer Details:</p>
-          <input
-            type="text"
-            placeholder="Input Address"
-            className="mr-10 px-10 py-3 rounded-md border-2 border-black"
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            className="px-10 py-3 rounded-md border-2 border-black"
-          />
-          <div className="mt-4">
-            <input type="checkbox" id="terms" className="hidden peer" />
-            <label htmlFor="terms" className="flex items-center cursor-pointer">
-              <input type="checkbox" defaultChecked className="checkbox" />
-              <p className="ml-2">
-                I agree to GlowCart{" "}
-                <span className="text-orange-400 cursor-pointer text-sm md:text-xl lg:text-xl">
-                  terms and conditions
-                </span>{" "}
-                together with its{" "}
-                <span className="text-orange-400 cursor-pointer">
-                  privacy policy
-                </span>
-              </p>
-            </label>
-          </div>
-          <div className="mt-4">
-            <input type="checkbox" id="promotions" className="hidden peer" />
-            <label
-              htmlFor="promotions"
-              className="flex items-center cursor-pointer"
-            >
-              <input type="checkbox" defaultChecked className="checkbox" />
-              <p className="ml-2">
-                I would like to receive promotional / marketing emails
-              </p>
-            </label>
-          </div>
-          <div className="mt-10 ">
-            <label>Delivery Date:</label>
+        <form>
+          <section className="ml-10 lg:justify-center lg:items-center lg:ml-52">
+            <p className="font-semibold mx-4 inline">Customer Details:</p>
             <input
-              type="date"
-              className="py-4 px-20 md:px-40 md:ml-5 md:mt-5 border-2 border-black rounded-md"
+              type="text"
+              placeholder="Input Address"
+              className="mr-10 px-10 py-3  rounded-md border-2 border-black"
             />
-            <br />
-            <label>Payment Method:</label>
-            <select
-              className="py-4 md:px-20  md:ml-5 md:mt-5 border-2 border-black rounded-md"
-              onChange={handleCurrencyChange}
-            >
-              <option value="">Select Currency</option>
-              <option value="USD">USD $</option>
-              <option value="EUR">Euro EUR €</option>
-              <option value="GBP">British Pound Sterling GBP £</option>
-              <option value="JPY">Japanese Yen JPY ¥</option>
-              <option value="NGN">Nigerian Naira NGN ₦</option>
-              <option value="AUD">Australian Dollar AUD A$</option>
-              <option value="CAD">Canadian Dollar CAD C$</option>
-              <option value="CHF">Swiss Franc CHF</option>
-              <option value="CNY">Chinese Yuan CNY ¥</option>
-              <option value="KES">Kenyan Shilling KES KSh</option>
-              <option value="MXN">Mexican Peso MXN $</option>
-              <option value="SGD">Singapore Dollar SGD S$</option>
-              <option value="HKD">Hong Kong Dollar HKD HK$</option>
-              <option value="NZD">New Zealand Dollar NZD NZ$</option>
-              <option value="KRW">South Korean Won KRW ₩</option>
-              <option value="SEK">Swedish Krona SEK kr</option>
-              <option value="NOK">Norwegian Krone NOK kr</option>
-              <option value="DKK">Danish Krone DKK kr</option>
-              <option value="TRY">Turkish Lira TRY ₺</option>
-              <option value="AED">United Arab Emirates Dirham AED د.إ</option>
-              <option value="INR">Indian Rupee INR ₹</option>
-              <option value="BRL">Brazilian Real BRL R$</option>
-              <option value="ZAR">South African Rand ZAR R</option>
-              <option value="RUB">Russian Ruble RUB ₽</option>
-            </select>
-          </div>
-          <div className="mt-10">
-            <button
-              onClick={handlePayment}
-              disabled={!currencySelected || isLoading}
-              className={`px-24 py-5 rounded-md border-none text-white hover:bg-orange-400 bg-orange-500 flex justify-center items-center ${
-                (!currencySelected || isLoading) &&
-                "cursor-not-allowed opacity-50"
-              }`}
-            >
-              {isLoading ? (
-                <span className="loading loading-dots loading-md"></span>
-              ) : (
-                "Pay"
-              )}
-            </button>
-          </div>
-        </section>
+            <input
+              type="text"
+              placeholder="Email"
+              className="px-10 py-3 mt-5 md:mt-0 rounded-md border-2 border-black"
+            />
+            <div className="mt-4">
+              <input type="checkbox" id="terms" className="hidden peer" />
+              <label
+                htmlFor="terms"
+                className="flex items-center cursor-pointer"
+              >
+                <input type="checkbox" defaultChecked className="checkbox" />
+                <p className="ml-2">
+                  I agree to GlowCart{" "}
+                  <span className="text-orange-400 cursor-pointer text-sm md:text-xl lg:text-xl">
+                    terms and conditions
+                  </span>{" "}
+                  together with its{" "}
+                  <span className="text-orange-400 cursor-pointer">
+                    privacy policy
+                  </span>
+                </p>
+              </label>
+            </div>
+            <div className="mt-4">
+              <input type="checkbox" id="promotions" className="hidden peer" />
+              <label
+                htmlFor="promotions"
+                className="flex items-center cursor-pointer"
+              >
+                <input type="checkbox" defaultChecked className="checkbox" />
+                <p className="ml-2">
+                  I would like to receive promotional / marketing emails
+                </p>
+              </label>
+            </div>
+            <div className="mt-10 ">
+              <label>Delivery Date:</label>
+              <input
+                type="date"
+                className="py-4 px-20 md:px-40 md:ml-5 md:mt-5 border-2 border-black rounded-md"
+              />
+              <br />
+              <label>Payment Method:</label>
+              <select
+                className="py-4 md:px-20  md:ml-5 md:mt-5 border-2 border-black rounded-md"
+                onChange={handleCurrencyChange}
+              >
+                <option value="">Select Currency</option>
+                <option value="USD">USD $</option>
+                <option value="EUR">Euro EUR €</option>
+                <option value="GBP">British Pound Sterling GBP £</option>
+                <option value="JPY">Japanese Yen JPY ¥</option>
+                <option value="NGN">Nigerian Naira NGN ₦</option>
+                <option value="AUD">Australian Dollar AUD A$</option>
+                <option value="CAD">Canadian Dollar CAD C$</option>
+                <option value="CHF">Swiss Franc CHF</option>
+                <option value="CNY">Chinese Yuan CNY ¥</option>
+                <option value="KES">Kenyan Shilling KES KSh</option>
+                <option value="MXN">Mexican Peso MXN $</option>
+                <option value="SGD">Singapore Dollar SGD S$</option>
+                <option value="HKD">Hong Kong Dollar HKD HK$</option>
+                <option value="NZD">New Zealand Dollar NZD NZ$</option>
+                <option value="KRW">South Korean Won KRW ₩</option>
+                <option value="SEK">Swedish Krona SEK kr</option>
+                <option value="NOK">Norwegian Krone NOK kr</option>
+                <option value="DKK">Danish Krone DKK kr</option>
+                <option value="TRY">Turkish Lira TRY ₺</option>
+                <option value="AED">United Arab Emirates Dirham AED د.إ</option>
+                <option value="INR">Indian Rupee INR ₹</option>
+                <option value="BRL">Brazilian Real BRL R$</option>
+                <option value="ZAR">South African Rand ZAR R</option>
+                <option value="RUB">Russian Ruble RUB ₽</option>
+              </select>
+            </div>
+            <div className="mt-10">
+              <button
+                onClick={handlePayment}
+                disabled={!currencySelected || isLoading}
+                className={`px-24 py-5 rounded-md border-none text-white hover:bg-orange-400 bg-orange-500 flex justify-center items-center ${
+                  (!currencySelected || isLoading) &&
+                  "cursor-not-allowed opacity-50"
+                }`}
+              >
+                {isLoading ? (
+                  <span className="loading loading-dots loading-md"></span>
+                ) : (
+                  "Pay"
+                )}
+              </button>
+            </div>
+          </section>
+        </form>
       </div>
       <Footer />
 
